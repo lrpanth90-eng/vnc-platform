a49d4abddf340e2f16a8c707053cf82a1fdc2225
lrpanth90-eng <lrpanth90@gmail.com>
2026-02-05 08:36:33 +0530

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PlatformGuard } from './guards/platform.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableShutdownHooks();
+  app.useGlobalGuards(app.get(PlatformGuard));
+
   app.enableCors({
     origin: true,
     credentials: true
